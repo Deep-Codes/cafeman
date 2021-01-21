a43c3dfb28a8b2da94c0be199999f98b4aa44434
Deepankar <61158210+Deep-Codes@users.noreply.github.com>
2021-01-21 16:59:35 +0530

    ✨ Create Menu with Rails Form Helper

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,4 +2,12 @@ class MenusController < ApplicationController
   # ? => /menus
   def index
   end
+
+  # ? => /menus
+  # ? POST
+  def create
+    name = params[:name]
+    new_menu = Menu.create!(name: name)
+    redirect_to menus_path
+  end
 end

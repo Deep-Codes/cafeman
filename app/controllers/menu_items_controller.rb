6c3ef57f67ea90f919bbed5fb9561f087cbaee4d
Deepankar <61158210+Deep-Codes@users.noreply.github.com>
2021-01-21 15:31:38 +0530

    ✨ API: Adding and Deleting MenuItems

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,5 +1,28 @@
 class MenuItemsController < ApplicationController
+
+  # ! For Testing Purpose ONLY
+  skip_before_action :verify_authenticity_token
+
   # ? => /menu_items
   def index
+  end
+
+  # ? => /menu_items
+  # ? => POST
+  def create
+    menu_id = params[:menu_id]
+    name = params[:name]
+    description = params[:description]
+    price = params[:price]
+    new_menu_item = MenuItem.create!(menu_id: menu_id, name: name, description: description, price: price)
+    render plain: "MenuItem Created with the name : #{new_menu_item[:name]}"
+  end
+
+  # ? => DELETE
+  def destroy
+    menu_item_id = params[:id]
+    menu_item = MenuItem.find(menu_item_id)
+    menu_item.destroy
+    render plain: "MenuItem Destroyed with the name : #{menu_item[:name]}"
   end
 end

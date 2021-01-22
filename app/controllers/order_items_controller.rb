c8c8656883e0cc8d269c0bd5cff83628739ddafa
Deepankar <61158210+Deep-Codes@users.noreply.github.com>
2021-01-22 21:30:49 +0530

    🕹 Add / Update OrderItem

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -8,12 +8,35 @@ class OrderItemsController < ApplicationController
     menu_item_id = params[:menu_item_id]
     menu_item_name = params[:menu_item_name]
     menu_item_price = params[:menu_item_price]
-    OrderItem.create!(
-      order_id: order_id,
-      menu_item_id: menu_item_id,
-      menu_item_name: menu_item_name,
-      menu_item_price: menu_item_price,
-    )
-    render plain: "OrderItem Added: #{menu_item_name}"
+
+    item_existing_in_cart = OrderItem.where(order_id: order_id).find_by(menu_item_id: menu_item_id)
+
+    # * If Does not OrderItem Exists
+    # * Add a Fresh new OrderItem in the cart
+    if item_existing_in_cart == nil
+      OrderItem.create!(
+        order_id: order_id,
+        menu_item_id: menu_item_id,
+        menu_item_name: menu_item_name,
+        menu_item_price: menu_item_price,
+        count: 1,
+      )
+      redirect_to orders_path
+      # * If  OrderItem Exists
+      # * Update the Count of item
+    else
+      id = item_existing_in_cart.id
+      count = item_existing_in_cart.count
+      OrderItem.find(id).update!(count: count + 1)
+      redirect_to orders_path
+    end
+  end
+
+  def update
+    id = params[:id]
+    order_menu_count = params[:count]
+    new_order_item = OrderItem.find(id)
+    new_order_item.update!(count: order_menu_count)
+    # redirect_to orders_path
   end
 end

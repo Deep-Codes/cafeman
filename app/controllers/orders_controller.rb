be9c32779e621d93852efbb8a30b9d2d9118a939
Deepankar <61158210+Deep-Codes@users.noreply.github.com>
2021-01-22 17:35:14 +0530

    ✨ Basic Version of Order Cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,9 @@ class OrdersController < ApplicationController
 
   # ? => /orders
   def index
-    session[:current_order_id] = 10
+    if session[:current_order_id] == nil
+      new_order = Order.create(user_id: 2, delivered_at: "")
+      session[:current_order_id] = new_order.id
+    end
   end
 end

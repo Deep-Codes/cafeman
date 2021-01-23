7feb5ffa208ee4be8a294e0f52cdb5ae4ac8c47c
Deepankar <61158210+Deep-Codes@users.noreply.github.com>
2021-01-23 10:42:47 +0530

    🕹 Place Order  Cart => Queue

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,21 @@
 class OrdersController < ApplicationController
 
+  # ! For Testing Purpose ONLY
+  skip_before_action :verify_authenticity_token
+
   # ? => /orders
   def index
     if session[:current_order_id] == nil
       new_order = Order.create(user_id: 2, delivered_at: "", order_status: "cart")
       session[:current_order_id] = new_order.id
     end
+  end
+
+  # ? => orders/:id
+  # ? => updating order_status
+  def update
+    id = params[:id]
+    order_status = params[:order_status]
+    Order.find(id).update!(order_status: "queue")
   end
 end

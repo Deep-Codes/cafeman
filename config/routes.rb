92ff2111ca0723004f6e7f6e469db5666b82f8f6
Deepankar <61158210+Deep-Codes@users.noreply.github.com>
2021-01-23 14:59:43 +0530

    ⚡️ User SigIn API

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get "/" => "home#index"
+  get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin" => "sessions#create", as: :sessions
+  # delete "/signout" => "sessions#destroy", as: :destroy_session
+  # post "users/login", to: "users#login"
+
   resources :menus
   resources :menu_items
   resources :active_menus

73843ed3244092f373767dce4bdffe60e8f60c2a
Deepankar <61158210+Deep-Codes@users.noreply.github.com>
2021-01-23 15:54:53 +0530

    🥳 Adding Signing-In Updating Sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,15 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_logged_in
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      render plain: "Correct Password"
+      session[:current_user_id] = user.id
+      session[:current_user_role] = user.role
+      redirect_to "/"
     else
       render plain: "Incorrect Passowrd"
     end

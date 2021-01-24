8c65bfbd8395e514ece07da888d6418e00a3ad93
Deepankar <61158210+Deep-Codes@users.noreply.github.com>
2021-01-24 09:10:02 +0530

    🎉 Validation and Flash Errors in Auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,16 +10,20 @@ class UsersController < ApplicationController
     email = params[:email]
     password = params[:password]
     role = params[:role]
-    new_user = User.create!(
+    new_user = User.new(
       name: name,
       email: email,
       password: password,
       role: role,
     )
+    new_user.save
     if new_user.save
       session[:current_user_id] = new_user.id
       session[:current_user_role] = new_user.role
       redirect_to "/"
+    else
+      flash[:error] = new_user.errors.full_messages.join(" , ")
+      redirect_to new_user_path
     end
   end
 end

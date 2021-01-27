5380f98e980003685faf9a13c8fa99e35917597e
Deepankar <61158210+Deep-Codes@users.noreply.github.com>
2021-01-27 12:34:29 +0530

    🧪 OrderItem CRUD Tests

--- test/helpers/helpers.rb
+++ test/helpers/helpers.rb
@@ -12,3 +12,7 @@ def create_order
   new_order = Order.create(user_id: User.first.id, delivered_at: "", order_status: "cart")
   new_order.id
 end
+
+def add_menu_item(id, item)
+  new_menu_item = MenuItem.create(menu_id: id, name: item.name, description: item.description, price: item.price)
+end
